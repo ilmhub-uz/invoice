491e4bbcc81dcb6a38da64a7ccde0589699b6f0a
Furqat-DevDo <furqata93@gmail.com>
2022-02-08 18:08:57 +0500

    ContactViewModel added

--- webapp/Controllers/ContactController.cs
+++ webapp/Controllers/ContactController.cs
@@ -2,5 +2,5 @@ namespace webapp.Controllers;
 
 public class ContactController
 {
-
+   
 }
--- webapp/ViewModel/ContactViewModel.cs
+++ webapp/ViewModel/ContactViewModel.cs
@@ -2,5 +2,25 @@ namespace webapp.ViewModels;
 
 public class ContactViewModel
 {
+   [Key]
+   public Guid Id { get; set; }   
 
+    [Required(ErrorMessage = "ism-sharfini kiritish shart!")]
+    [Display(Name = "Ism")]
+   public string Name { get; set; }  
+
+    [Required(ErrorMessage = "Manzilni kiriting")]
+    [Display(Name = "Ism")]  
+   public string Address { get; set; }  
+
+   [Required]
+   [Column(TypeName = "nvarchar(15)")] 
+   public string Phone { get; set; }   
+
+   [Required]
+   [Column(TypeName = "nvarchar(128)")] 
+   public string Email { get; set; } 
+
+   public Guid OwnerId { get; set; }   
+   public virtual AppUser Owner { get; set; }  
 }
