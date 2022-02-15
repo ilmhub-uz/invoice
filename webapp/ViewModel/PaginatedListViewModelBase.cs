d017171924e2da66ed0cec4987dafa8681962214
Wahid Abduhakimov <wakhid2802@gmail.com>
2022-02-15 23:54:03 +0500

    Create PaginatedListViewModelBase.cs

---
+++ webapp/ViewModel/PaginatedListViewModelBase.cs
@@ -0,0 +1,10 @@
+namespace webapp.ViewModels;
+
+public class PaginatedListViewModel<T>
+{
+    public List<T> Items { get; set; }
+    public uint CurrentPage { get; set; }
+    public uint Limit { get; set; }
+    public uint TotalPages { get; set; }
+    public uint TotalItems { get; set; }
+}
